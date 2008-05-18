415c906785254d7f99da1178060034941eb08cbc
Mark Chadwick <mchadwick@mchadwick-desktop.(none)>
2008-05-17 21:14:15 -0400

    Shuffling files around to be proper "clone"

--- src/proxe/graphics/Graphics.hx
+++ src/proxe/graphics/Graphics.hx
@@ -1,287 +1,242 @@
 package proxe.graphics;
 
-import proxe.Color;
-
-enum GraphicType {
-    MOCK;
-}
-
-enum ShapeType {
-    POINTS;
-    POLYGON;
-    LINES;
-    TRIANGLES;
-    TRIANGLE_FAN;
-    TRIANGLE_STRIP;
-    QUADS;
-    QUAD_STRIP;
-}
-
-enum ShapeClosingType {
-    OPEN;
-    CLOSE;
-}
-
-enum EllipseMode {
-    CENTER;
-    RADIUS;
-    CORNER;
-    CORNERS;
-}
-
 class Graphics {
 
-    /**
-     * The type of shape the graphics is currently drawing.  If the graphics is
-     * not currently drawing anything, this should be null.  Possible values for
-     * this variable can be found in the above `ShapeType` enum.
-     */
-    public var currentShapeType:ShapeType;
-
-    /**
-     * Current shape depth for this graphics
-     */
-    public var shapeDepth:Int;
-
-    /**
-     * Current number of calls to the `vertex` method since the last
-     * `beginShape`
-     */
-    public var vertexDepth:Int;
-
-    /**
-     * List of verticies collected between calling `beingShape()` and calling
-     * `endShape()`.
-     */
-    var vertices:Array<Array<Float>>;
-
-    private var fillColor:Color;
-    private var strokeColor:Color;
-    private var strokeWidth:Float;
-
-    public var ellipseMode:EllipseMode;
-
     ////////////////////////////////////////////////////////////////////////////
-    // Temporary Methods
-    public function background(color:Color) {
-        trace("Graphics.background() should not be called.");
-        throw("Graphics.background() should not be called.");
-    }
+    // CONSTANTS
+    
+    /*
+     * Transformed values (to be used in rendering)
+     */
+    public static var X:Int = 0;
+    public static var Y:Int = 1;
+    public static var Z:Int = 2;
 
-    public function point(x:Float, y:Float, ?z:Float) {
-        trace("Graphics.point() should not be called.");
-        throw("Graphics.point() should not be called.");
-    }
+    /*
+     * Actual RGBA, after lighting.  Fill stored here, transform in place.
+     */
+    public static var R:Int = 3;
+    public static var G:Int = 4;
+    public static var B:Int = 5;
+    public static var A:Int = 6;
 
-    public function ellipse(x:Float, y:Float, width:Float, height:Float) {
-        if(ellipseMode == null) {
-            ellipseMode = CENTER;
-        }
-        
-        switch ellipseMode {
-            case CENTER:
-                drawEllipse(x-(width/2), y-(height/2), width, height);
-            case CORNER:
-                drawEllipse(x, y, width, height);
-            default:
-                trace("Unknown ellipseMode: "+ ellipseMode);
-                throw("Unknown ellipseMode: "+ ellipseMode);
-        }
-    }
+    /*
+     * Values that need to transformation, but will be used in rendering
+     */
+    public static var U:Int = 7;
+    public static var V:Int = 8;
 
-    public function drawEllipse(x:Float, y:Float, width:Float, height:Float) {
-        trace("drawEllipse not implemented");
-        throw("drawEllipse not implemented");
-    }
+    /*
+     * Incoming values, raw and untransformed (won't be used in rendering)
+     */
+    public static var MX:Int = 9;
+    public static var MY:Int = 10;
+    public static var MZ:Int = 11;
 
-    ////////////////////////////////////////////////////////////////////////////
-    // Vertex Methods
+    /*
+     * Stroke ARGB values
+     */
+    public static var SR:Int = 12;
+    public static var SG:Int = 13;
+    public static var SB:Int = 14;
+    public static var SA:Int = 15;
+
+    /*
+     * Stroke Weight
+     */
+    public static var SW:Int = 16;
+
+    /*
+     * Not ued in rendering, only used for calculating colors.
+     */
+    public static var NX:Int = 17;
+    public static var NY:Int = 18;
+    public static var NZ:Int = 19;
+
+    /*
+     * View space coordinates
+     */
+    public static var VX:Int = 20;
+    public static var VY:Int = 21;
+    public static var VZ:Int = 22;
+    public static var VW:Int = 23;
+
+    /*
+     * Ambient color (usually to be kept the same as diffuse).  fill(_) sets
+     * both ambient and diffuse.
+     */
+    public static var AR:Int = 24;
+    public static var AG:Int = 25;
+    public static var AB:Int = 26;
+
+    /*
+     * Diffuse is shared with fill
+     */
+    public static var DR:Int = R;
+    public static var DG:Int = G;
+    public static var DB:Int = B;
+    public static var DA:Int = A;
+
+    /*
+     * Specular (by default kept white)
+     */
+    public static var SPR:Int = 27;
+    public static var SPG:Int = 28;
+    public static var SPB:Int = 29;
+    public static var SPA:Int = 30;
+
+    /*
+     * Shininess
+     */
+    public static var SHINE:Int = 31;
+
+    /*
+     * Emissive (by default kept black)
+     */
+    public static var ER:Int = 32;
+    public static var EG:Int = 33;
+    public static var EB:Int = 34;
+
+    /*
+     * Has the vertex been let yet?
+     */
+    public static var BEEN_LIT:Int = 35;
+
+    /*
+     * Count of the vertices
+     */
+    public static var VERTEX_FIELD_COUNT:Int = 36;
+
+    /*
+     * Line and Triangle fields (note how these overlap)
+     */
+    public static var INDEX:Int = 0;
+    public static var VERTEX1:Int = 1;
+    public static var VERTEX2:Int = 2;
+    public static var VERTEX3:Int = 3;
+    public static var TEXTURE_MODE:Int = 4;
+    public static var STROKE_MODE:Int = 3;
+    public static var STROKE_WEIGHT:Int = 4;
+
+    public static var LINE_FIELD_COUNT:Int = 5;
+    public static var TRIANGLE_FIELD_COUNT:Int = 5;
+
+    public static var TRI_DIFFUSE_R:Int = 0;
+    public static var TRI_DIFFUSE_G:Int = 1;
+    public static var TRI_DIFFULE_B:Int = 2
+    public static var TRI_DIFFUSE_A:Int = 3;
+
+    public static var TRI_SPECULAR_A:Int = 4;
+    public static var TRI_SPECULAR_G:Int = 5;
+    public static var TRI_SPECULAR_B:Int = 6;
+    public static var TRI_SPECULAR_A:Int = 7;
+
+    public static var TRIANGLE_COLOR_COUNT:Int = 8;
+
+    /*
+     * Normal modes for lighting, these have the uglier naming becase the
+     * constants are never seen by users
+     *
+     * TODO: Yuck!  See above.
+     */
     
     /**
-     * Allows the graphics to start accepting vertices for a given shape.  Code
-     * using this graphics library should be aware that before sending vertices
-     * to a `Graphics`, a `beginShape` call must be made.
-     *
-     * By default, the shapeType will be POLYGON.
-     *
-     * If `beginShape` is called while another shape is in progress, this method
-     * will throw an exception.
-     * 
-     * The following types of shapes are enabled by default (but, of couse,
-     * could be overridden in a child class).
-     *
-     * == POINTS ==
-     *  Will not draw connecting lines, or fill the contained area.  Will simply
-     *  draw single points at the passes vertices
-     * 
-     * == POLYGON ==
-     *  Default drawing style.  Given a set of points, it create a path through
-     *  the points.  If a fillColor is given, it will close the path (regardless
-     *  of `closeShape` being called with `CLOSE`), and fill the interior. 
-     * 
-     * == LINES ==
-     *  Will take pairs of `vertex` method calls, and draw desperate lines
-     *  between them.  Take the following code:
-     *
-     *      beginShape(LINES);
-     *      vertex(30, 20);
-     *      vertex(85, 20);
-     *      vertex(85, 75);
-     *      vertex(30, 75);
-     *      endShape();
-     *
-     * Will draw two lines.  One from {30,20} to {85, 20}, and another line
-     * between {85,75} and {30,75}, with nothing connecting the two.
-     * 
-     * == TRIANGLES ==
-     * == TRIANGLE_FAN ==
-     * == TRIANGLE_STRIP ==
-     * == QUADS ==
-     * == QUAD_STRIP ==
-     *
-     * @param shapeType:ShapeType   defines the type of shape to be drawn (via
-     *                              following vertex() method calls). Valid
-     *                              values are POINTS, POLYGON, LINES,
-     *                              TRIANGLES, TRIANGLE_FAN, TRIANGLE_STRIP,
-     *                              QUADS, and QUAD_STRIP.  See the above
-     *                              documentation for more information.
+     * Normal calculated per triangle
      */
-    public function beginShape(?shapeType:ShapeType) {
-        if(shapeType == null) {
-            shapeType = POLYGON;
-        }
-
-        #if neko
-        if(shapeDepth == null) { shapeDepth = 0; }
-        #end
-        
-        shapeDepth++;
-        vertexDepth = 0;
-        
-        if(shapeDepth > 1) {
-            throw "Already drawing "+ currentShapeType +", cannot draw new "
-                + shapeType +".";
-        }
-
-        this.currentShapeType = shapeType;
-        this.vertices = new Array<Array<Float>>();
-    }
+    public static var AUTO_NORMAL:Int = 0;
 
     /**
-     * This is the companion to `beginShape`, and may only be called after
-     * `beginShape`.  If a `beginShape` hasn't been called, this will throw an
-     * exception.
-     *
-     * @param shapeClosingType  Either null or a member of `ShapeClosingType`.
-     *                          If passed `CLOSE`, it will connect the last
-     *                          vertex of the draw with the first, and attempt
-     *                          to fill the internals.
+     * One normal manually specified per shape
      */
-    public function endShape(?shapeClosingType:ShapeClosingType) {
-        if(shapeClosingType == null) {
-            shapeClosingType = OPEN;
-        }
-
-        if(shapeClosingType == CLOSE && vertices.length > 1) {
-            var v:Array<Float> = vertices[0];
-            vertices.push(v);
-        }
-
-        drawVertices(vertices, currentShapeType, shapeClosingType);
-        
-        shapeDepth--;
-        vertexDepth = 0;
-        currentShapeType = null;
-    }
-
-    public function vertex(x:Float, y:Float, ?z:Float) {
-        vertexDepth++;
-        
-        var point:Array<Float> = [x, y, z];
-        vertices.push(point);
-    }
-
-    ////////////////////////////////////////////////////////////////////////////
-    // Shape Methods
-    public function triangle(x1:Float, y1:Float, x2:Float, y2:Float,
-        x3:Float, y3:Float) {
-
-        beginShape(POLYGON);
-        vertex(x1, y1);
-        vertex(x2, y2);
-        vertex(x3, y3);
-        endShape(CLOSE);
-    }
-
-    public function rect(x:Float, y:Float, width:Float, height:Float) {
-        beginShape(POLYGON);
-        vertex(x, y);
-        vertex(x+width, y);
-        vertex(x+width, y+height);
-        vertex(x, y+height);
-        endShape(CLOSE);
-    }
+    public static var MANUAL_SHAPE_NORMAL:Int = 1;
 
     /**
-     * TODO: Only handles 2D lines
+     * Normals specified for each shape vertex
      */
-    public function line(x1:Float, y1:Float, z1:Float,
-        x2:Float, ?y2:Float, ?z2:Float) {
-
-        beginShape(LINES);
-        vertex(x1, y1);
-        vertex(z1, x2);
-        endShape();
-    }
+    public static var MANUAL_VERTEX_NORMAL:Int = 2;
 
     ////////////////////////////////////////////////////////////////////////////
-    // Color Methods
-    public function stroke(color:Color) {
-        this.strokeColor = color;
-    }
+    // Instance Fields
 
-    public function fill(color:Color) {
-        this.fillColor = color;
-    }
+    /**
+     * Width minus one (useful for many calculations)
+     */
+    public var width1:Int;
 
-    public function strokeWeight(strokeWidth:Float) {
-        this.strokeWidth = strokeWidth;
-    }
+    /**
+     * Height minus one (useful for many calculations)
+     */
+    public var height1:Int;
 
-    ////////////////////////////////////////////////////////////////////////////
-    // Matrix Methods
-    public function pushMatrix() {
-        if (matrixStackDepth+1 == MATRIX_STACK_DEPTH) {
-            trace("too many calls to pushMatrix()");
-            throw("too many calls to pushMatrix()");
-        }
-        //float mat[] = matrixStack[matrixStackDepth];
-        var mat:Array<Float> = matrixStack[matrixStackDepth];
-        
-        // mat[0] = m00;
-        // mat[1] = m01; mat[2] = m02;
-        mat[0] =
-    mat[3] = m10; mat[4] = m11; mat[5] = m12;
-    matrixStackDepth++;
-  }
+    /**
+     * width * height (useful for many calculations)
+     */
+    public var pixelCount:Int;
 
+    /**
+     * True if defaults() has been called a first time
+     */
+    public var defaultsInited:Int;
 
-  public function popMatrix() {
-    if (matrixStackDepth == 0) {
-      throw new RuntimeException("too many calls to popMatrix() " +
-                                 "(and not enough to pushMatrix)");
-    }
-    matrixStackDepth--;
-    float mat[] = matrixStack[matrixStackDepth];
-    m00 = mat[0]; m01 = mat[1]; m02 = mat[2];
-    m10 = mat[3]; m11 = mat[4]; m12 = mat[5];
-  }
+    /**
+     * true if in-between beginDraw() and endDraw()
+     */
+    public var insdeDraw:Bool;
+
+    /**
+     * True if in the midst of resize (no drawing can take place)
+     */
+    public var insideResize:Bool;
 
     ////////////////////////////////////////////////////////////////////////////
-    // Abstract Methods
-    public function drawVertices(verticies:Array<Array<Float>>,
-        openShapeType:ShapeType, closeShapeType:ShapeClosingType) {
-        trace("Unimplemented: Graphics.drawVertices");
-        throw "Unimplemented: Graphics.drawVerticies";
-    }
+    // Color Fields
+
+    /**
+     * The current color mode
+     * 
+     * @default RGB
+     */
+    public var colorMode:Int;
+
+    /**
+     * Max value for red (or hue) set by colorMode
+     *
+     * @default 255
+     */
+    public var colorModeX:Float;
+
+    /**
+     * Max value for green (or saturation) set by colorMode
+     *
+     * @default 255
+     */
+    public var colorModeY:Float;
+
+    /**
+     * Max value for blue (or value) set by colorMode
+     *
+     * @default 255
+     */
+    public var colorModeZ:Float;
+
+    /**
+     * Max value for alpha set by colorMode
+     *
+     * @default 255
+     */
+    public var colorModeA:Float;
+
+    /**
+     * True if colors are not in the range 0..1
+     * 
+     * @default true
+     */
+    private var colorScale:Bool;
+
+    /**
+     * True if colorMode(RGB, 255)
+     * 
+     * @default true
+     */
+    private var colorRgb255:Bool;  
 }
